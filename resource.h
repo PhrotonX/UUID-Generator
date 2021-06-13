4d8f3bfd3df701d012ec964f06c46a88b45cd5ef
PhrotonX <cpascual185@gmail.com>
2021-06-13 14:18:11 +0800

    Build 59 - GUI Reset

--- resource.h
+++ resource.h
@@ -1,11 +1,34 @@
-#ifndef RESOURCE_H_INCLUDED
-#define RESOURCE_H_INCLUDED
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
 
-#define IDC_BUTTON_GENERATE     20000
-#define IDC_STATIC_TEXT         30000
-#define IDC_TIME_LOW            30001
-#define ID_FILE_EXIT            40000
-#define ID_EDIT_COPYCONTENTS    40001
-#define ID_HELP_ABOUT           40002
-
-#endif // RESOURCE_H_INCLUDED
+#define IDD_DIALOG1                             100
+#define IDD_DIALOG2                             101
+#define IDS_TIME_LOW                            40000
+#define ID_ABOUT_VIEW_REPO                      40000
+#define ID_COPY                                 40000
+#define ID_ABOUT_UPDATE                         40001
+#define ID_ABOUT_OK                             40002
+#define IDC_UUID_EDIT                           40013
+#define ID_GENERATE                             40016
+#define IDS_TIME_MID                            40017
+#define ID_ABOUT                                40018
+#define IDS_TIME_HI_AND_VERSION                 40019
+#define IDS_VERSION                             40020
+#define IDS_VARIANT                             40021
+#define IDS_CLOCK_SEQ_HI_AND_RES_CLOCK_SEQ_LOW  40022
+#define IDS_NODE                                40023
+#define IDS_TIMESTAMP_TIME_HIGH_AND_VERSION     40024
+#define IDS_TIMESTAMP_TIME_MID                  40025
+#define IDS_TIMESTAMP_TIME_LOW                  40026
+#define IDC_ADV_RS_UCV                          40027
+#define IDC_ADV_RS_MS                           40028
+#define IDC_ADV_RS_NCS                          40029
+#define IDC_ADV_VS_UD                           40030
+#define IDC_ADV_VS_UD_HEX                       40031
+#define IDC_ADV_VS_DV                           40032
+#define IDC_ADV_VS_RAND                         40033
+#define IDC_OPT_UUID_UPL                        40034
+#define IDC_OPT_UUID_LWL                        40035
+#define IDC_OPT_UUID_USE_HYPENS                 40036
+#define IDC_OPT_UUID_USE_BRACES                 40037
